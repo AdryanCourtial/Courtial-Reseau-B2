9f0971dabbe31a6cb4fcd8b690fe2f3e43e759b9
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2023-11-26 21:02:05 +0100

    pt2

--- tp4 dev/bs_client_I3.py
+++ tp4 dev/bs_client_I3.py
@@ -15,13 +15,13 @@ try :
     print(f"Connecté avec succès au serveur {host} sur le port {port}")
     time.sleep(2)
 
-    answer = input("Que veux-tu envoyer au serveur ?")
+    answer = input("Que veux-tu envoyer au serveur ? \n")
 
-    p = re.compile("meo|waf")
+    p = re.compile('meo|waf')
     if type(answer) is not str:
-        raise TypeError(b"Ecrit une une phrase !")
+        raise TypeError("Ecrit une une phrase !")
     elif p.match(answer == None):
-        raise TypeError(b"Tu n'es pas le bienvenue Ici.")
+        raise TypeError("Tu n'es pas le bienvenue Ici.")
     else:
         s.send(answer.encode())
         data = s.recv(512)
