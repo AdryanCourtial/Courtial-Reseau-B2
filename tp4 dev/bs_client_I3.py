a46bcf780d75ac445548db7a321774919a5ae51c
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2023-11-26 20:50:29 +0100

    pt2

---
+++ tp4 dev/bs_client_I3.py
@@ -0,0 +1,34 @@
+import socket
+import sys
+import time
+import re
+
+host = '10.1.1.11' # IP du serveur
+port = 13336       # Port choisir par le serveur
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+try :
+
+    s.connect((host, port))
+    print(f"Connecté avec succès au serveur {host} sur le port {port}")
+    time.sleep(2)
+
+    answer = input("Que veux-tu envoyer au serveur ?")
+
+    p = re.compile("meo|waf")
+    if type(answer) is not str:
+        raise TypeError("Ecrit une une phrase !")
+    elif p.match(answer == None):
+        raise TypeError("Tu n'est pas le bienvenue Ici.")
+
+    s.send(answer.encode())
+    
+    data = s.recv(512)
+    print(data)
+
+except Exception as e:
+    print(f"{e}")
+
+sys.exit()
