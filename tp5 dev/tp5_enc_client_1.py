5f612139df74edb2a196411df115e43ea3021b43
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2023-11-28 16:43:21 +0100

    o

--- tp5 dev/tp5_enc_client_1.py
+++ tp5 dev/tp5_enc_client_1.py
@@ -17,7 +17,7 @@ re = compile(r"^[0-9]{1,10} *[+x\-\*] *[0-9]{1,10}$")
 if re.match(calc):
     op = calc.split(sep='+')
     print(f"{op[0].encode('UTF-8')}")
-    if len(int(op[0].encode('UTF-8'))) > 2 or len(int(op[1].encode('UTF-8'))) > 2:
+    if len(str(op[0].encode('UTF-8'))) > 2 or len(str(op[1].encode('UTF-8'))) > 2:
         raise ValueError('Tu utilise des valeurs trop grandes')
     else:
         s.send(calc.encode('UTF-8'))
