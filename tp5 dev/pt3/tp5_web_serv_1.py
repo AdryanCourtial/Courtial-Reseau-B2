dc465dd7f3175c8e3be6a44ad211ff313a80c981
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2023-12-01 17:02:05 +0100

    o

--- tp5 dev/pt3/tp5_web_serv_1.py
+++ tp5 dev/pt3/tp5_web_serv_1.py
@@ -2,7 +2,7 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.connect(('10.1.1.11', 13336))
+s.connect(('10.1.1.11', 13337))
 
 s.listen(1)
 conn, addr = s.accept()
