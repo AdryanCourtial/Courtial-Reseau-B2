07a5db1efd3d082340a4008b1fe0145d6b27f3ad
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2023-12-03 14:46:36 +0100

    o

--- tp5 dev/pt3/tp5_web_serv_1.py
+++ tp5 dev/pt3/tp5_web_serv_1.py
@@ -4,15 +4,6 @@ from socketserver import TCPServer
 port = 13337
 adress = "10.1.1.11"
 
-handler = SimpleHTTPRequestHandler
-
-httpd = TCPServer((adress, port), handler)
-
-print(f"Serveur en cours d'exécution sur le port {port}")
-
-# Lancer le serveur
-httpd.serve_forever()
-
 class CustomHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         # Personnaliser la réponse pour la requête GET
@@ -21,3 +12,12 @@ class CustomHandler(SimpleHTTPRequestHandler):
             self.send_header("Reussi", "HTTP/1.0 200 OK\n\n<h1>Hello je suis un serveur HTTP</h1>")
             self.end_headers()
             self.wfile.write(b"Page d'accueil")
+            
+handler = CustomHandler
+
+httpd = TCPServer((adress, port), handler)
+
+print(f"Serveur en cours d'exécution sur le port {port}")
+
+# Lancer le serveur
+httpd.serve_forever()
