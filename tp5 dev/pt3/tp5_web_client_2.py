d2004a757a97acb3392e390cf98e89cf5f7ea7d9
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2023-12-03 14:29:46 +0100

    o

--- tp5 dev/pt3/tp5_web_client_2.py
+++ tp5 dev/pt3/tp5_web_client_2.py
@@ -1,11 +1,27 @@
-import socket
-import sys
+import http.client
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('10.1.1.11', 13337))
+# Spécifier l'adresse et le port du serveur auquel vous souhaitez vous connecter
+server_address = "10.1.1.11"
+server_port = 13337
 
-request = input("Quelle demmande veut-tu faire au server ?")
+# Créer une connexion HTTP avec le serveur
+conn = http.client.HTTPConnection(server_address, server_port)
 
+# Spécifier le chemin de la ressource que vous souhaitez récupérer (dans cet exemple, la racine '/')
+resource_path = '/'
 
+# Envoyer une requête GET au serveur
+conn.request("GET", resource_path)
 
-s.close()
+# Récupérer la réponse du serveur
+response = conn.getresponse()
+
+# Afficher le code d'état de la réponse
+print(f"Code d'état: {response.status}")
+
+# Afficher le contenu de la réponse
+print("Contenu de la réponse:")
+print(response.read().decode('utf-8'))
+
+# Fermer la connexion
+conn.close()
