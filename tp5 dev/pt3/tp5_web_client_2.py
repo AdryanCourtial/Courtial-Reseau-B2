03e0e89048e669689db32be119bcde52850ec2fe
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2023-12-03 14:34:01 +0100

    o

--- tp5 dev/pt3/tp5_web_client_2.py
+++ tp5 dev/pt3/tp5_web_client_2.py
@@ -1,17 +1,13 @@
 import http.client
 
-# Spécifier l'adresse et le port du serveur auquel vous souhaitez vous connecter
 server_address = "10.1.1.11"
 server_port = 13337
 
-# Créer une connexion HTTP avec le serveur
 conn = http.client.HTTPConnection(server_address, server_port)
 
-# Spécifier le chemin de la ressource que vous souhaitez récupérer (dans cet exemple, la racine '/')
-resource_path = '/'
 
 # Envoyer une requête GET au serveur
-conn.request("GET", resource_path)
+conn.request("GET", "/")
 
 # Récupérer la réponse du serveur
 response = conn.getresponse()
--- tp5 dev/pt3/tp5_web_serv_1.py
+++ tp5 dev/pt3/tp5_web_serv_1.py
@@ -1,17 +1,13 @@
-# Importer le module http.server
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
 
-# Spécifier le port sur lequel le serveur écoutera
 port = 13337
+adress = "10.1.1.11"
 
-# Créer une instance de SimpleHTTPRequestHandler
 handler = SimpleHTTPRequestHandler
 
-# Créer une instance de TCPServer liée à l'adresse locale et au port spécifié
-httpd = TCPServer(("10.1.1.11", port), handler)
+httpd = TCPServer((adress, port), handler)
 
-# Afficher un message pour indiquer que le serveur est en cours d'exécution
 print(f"Serveur en cours d'exécution sur le port {port}")
 
 # Lancer le serveur
