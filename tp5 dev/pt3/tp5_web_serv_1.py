9838100db845fa569ca5bdf5def38610bd82ee80
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2023-12-03 14:41:45 +0100

    o

--- tp5 dev/pt3/tp5_web_serv_1.py
+++ tp5 dev/pt3/tp5_web_serv_1.py
@@ -18,5 +18,5 @@ class CustomHandler(SimpleHTTPRequestHandler):
         # Personnaliser la réponse pour la requête GET
         if self.path == '/':
             self.send_response(200)
-            self.send_header("Content-type", "text/html")
+            self.send_header("Reussi", "HTTP/1.0 200 OK\n\n<h1>Hello je suis un serveur HTTP</h1>")
             self.end_headers()
