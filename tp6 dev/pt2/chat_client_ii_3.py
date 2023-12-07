d8753270c360fdcdb6d57fd07b5075ff074b169d
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2023-12-07 16:30:44 +0100

    o

--- tp6 dev/pt2/chat_client_ii_3.py
+++ tp6 dev/pt2/chat_client_ii_3.py
@@ -1,9 +1,9 @@
-import socket
 import asyncio
 import __future__
+import aioconsole
 
 ip = "10.1.1.11"
-port = 13337
+port = 13337  
 
 async def main():
     reader, writer = await asyncio.open_connection(host=ip, port=port)
@@ -13,6 +13,26 @@ async def main():
 
     entry = await reader.read(1024)
     print(entry.decode())
+
+    while True:
+        try:
+            async_input(writer)
+
+            async_receive(reader)
+            
+        except Exception: 
+            raise Exception
+        
+
+async def async_input(writer: asyncio.StreamWriter):
+    while True:
+        msg = await aioconsole.ainput("Que veut tu écrire ?")
+        writer.write((msg.encode()))
+
+async def async_receive(reader: asyncio.StreamReader):
+    while True:
+        msg = reader.read(1024)
+        print(msg.decode())
 
 if __name__ == "__main__":
     asyncio.run(main())
