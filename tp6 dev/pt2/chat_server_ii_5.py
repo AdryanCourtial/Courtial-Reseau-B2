3a23f66e280d30c1d9560fa5cf6b8e666cf279dc
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2023-12-08 16:45:56 +0100

    o

--- tp6 dev/pt2/chat_server_ii_5.py
+++ tp6 dev/pt2/chat_server_ii_5.py
@@ -20,7 +20,7 @@ async def handle_client_msg(reader, writer):
                     clients[addr]['w'] = writer
 
 
-            if "szlfkzifznznznfni|" in msg:
+            if "Hello|" in msg:
                 pseudo = msg[6::]
                 clients[addr]['pseudo'] = pseudo
                 for key in clients:
