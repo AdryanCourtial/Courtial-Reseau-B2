e5b9bc79586a89fa5d48b2fd4a8221ead7a0137d
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2023-12-08 17:07:46 +0100

    o

--- tp6 dev/pt2/chat_server_ii_5.py
+++ tp6 dev/pt2/chat_server_ii_5.py
@@ -15,7 +15,7 @@ async def handle_client_msg(reader, writer):
                         w = clients[key]["w"]
                         w.write(f"\n            {pseudo} C DECONNECTER \n".encode())
                         await w.drain()
-                        return
+                        return None
 
             addr = writer.get_extra_info("peername")
 
