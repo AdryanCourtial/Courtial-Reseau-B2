8e5cf59bf9266c746a7fecfc51d803c745e6bd61
AdryanCourtial <115459995+AdryanCourtial@users.noreply.github.com>
2023-12-11 14:44:47 +0100

    debutbonus

--- tp6 dev/Bonus/chat_server_ii_6.py
+++ tp6 dev/Bonus/chat_server_ii_6.py
@@ -47,7 +47,7 @@ async def handle_client_msg(reader, writer):
                         w = clients[key]["w"]
                         w.write(f"\033[{color}m{pseudo} a dit :    {msg}\033[0m".encode())
                         await w.drain()
-                        print(f"\033[{color}m{pseudo} a dit :    {msg}\033[0m")
+                        print(f"\033[{color}m{pseudo}\033[0m a dit :    {msg}")
             #One Envoie la donné a tout le monde 
 
         except Exception:
